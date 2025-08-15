9602804485d3beaf040d2c9a8653723b47569ab4
HoangDEV <nhh19072005@gmail.com>
2025-08-15 14:24:12 +0700

    done_week_2_crud_show

--- src/routers/page_manager/essay.ts
+++ src/routers/page_manager/essay.ts
@@ -3,7 +3,7 @@ import { methods as model_group } from '../../models/group';
 import { methods as model_essays, Essays } from '../../models/essays';
 import { authenticate } from '../../middware/auth'
 import express from "express"
-import { methods as model_user } from '../../models/user';
+import { methods as model_user, User } from '../../models/user';
 
 let route = express.Router();
 
@@ -25,12 +25,20 @@ route.get('/', authenticate.user_auth, async (req: any, res: Response): Promise<
             }
             return {
                 id: b.id,
-                text: contenObj.text,
-                image: JSON.stringify(contenObj.image)
+                users: {
+                    name: user.name,
+                    avatar: user.avatar
+                },
+                contens: {
+
+                    text: contenObj.text,
+                    image: JSON.stringify(contenObj.image)
+                },
+                updatedAt: b.updatedAt
             }
         })
-        console.log(essays_tranforme.length);
-        res.render('contens/page_manager/essay', { user: user.toJSON(), group: group.toJSON(), essay: essays_tranforme, isAdmin: isAdmin })
+        console.log(essays_tranforme);
+        res.render('contens/page_manager/essay', { user: user.toJSON(), group: group.toJSON(), essays: essays_tranforme, isAdmin: true })
 
     } catch (error) {
         console.log(error);
@@ -38,24 +46,42 @@ route.get('/', authenticate.user_auth, async (req: any, res: Response): Promise<
     }
 
 })
-// route.get('/admin', async (req: any, res: Response): Promise<any> => {
-//     try {
-//         const idGroup = req.session.currentGroupId;
-//         let groups = await model_group.findAll({ where: { id: idGroup } });
-//         let group = groups.map((b: any) => {
-//             b.toJSON();
-//         })
-//         let essays = await model_essays.select({ group_id: idGroup, scope: 'group' })
-//         let essay = essays.map((b: any) => {
-//             b.toJSON();
-//         })
-//         res.render('contens/page_manager/essay', { group: group, essay: essay })
+route.get('/admin', async (req: any, res: Response): Promise<any> => {
+    try {
 
-//     } catch (error) {
-//         console.log(error);
-//         res.status(500).json({ message: error })
-//     }
-// })
+        const isAdmin = req.session.admin;
+
+        //const idGroup = req.session.currentGroupId;
+        const idGroup = 1;
+        let group = await model_group.select(idGroup);
+        let essays = await Essays.findAll({
+            where: { group_id: idGroup, scope: 'group' },
+            include: [{
+                model: User,
+                as: 'users',
+                required: true
+            }]
+        });
+
+
+        const essays_tranforme = essays.map((b: any) => {
+            b = b.toJSON();
+            b.contens = JSON.parse(b.contens);
+            b.contens = JSON.parse(b.contens);
+            b.contens = {
+                text: b.contens.text,
+                image: JSON.stringify(b.contens.image)
+            }
+            return b;
+        })
+        console.log(essays_tranforme)
+        res.render('contens/page_manager/essay', { group: group?.toJSON?.(), essays: essays_tranforme })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error })
+    }
+})
 route.delete('/del', authenticate.user_auth, async (req: any, res: Response): Promise<any> => {
     const id = req.body;
     await model_essays.des(id);
