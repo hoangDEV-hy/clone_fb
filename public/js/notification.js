b8d218edfc4bcb2d7d622997cfb77805cd07967a
HoangDEV <nhh19072005@gmail.com>
2025-12-04 09:20:05 +0700

    done_create_user click vao thong bao

--- public/js/notification.js
+++ public/js/notification.js
@@ -10,7 +10,7 @@ const notification = {
             })
         }
     },
-    generate_notification(message, title = "Thông báo") {
+    generate_notification(message, title = "Thông báo", type) {
         const container = document.getElementById("toast-container");
 
         let toast = document.createElement('div');
@@ -18,6 +18,7 @@ const notification = {
         toast.setAttribute("role", "alert");
         toast.setAttribute("aria-live", "assertive");
         toast.setAttribute("aria-atomic", "true");
+        toast.dataset.toastType = type;
 
         toast.innerHTML = `
         <div class="toast-header">
@@ -31,12 +32,40 @@ const notification = {
 
         container.appendChild(toast);
 
+        toast.addEventListener('click', () => {
+            console.log("it runned")
+            if (type === "invited_joinChat") {
+                notification.handler_InvitedNotification();
+                toast.remove();
+            } else {
+                console.log("không tồn tại");
+            }
+        });
+
         const btToast = new bootstrap.Toast(toast);
         btToast.show();
 
         toast.addEventListener('hidden.bs.toast', () => {
             toast.remove();
         })
+    },
+    handler_InvitedNotification() {
+
+        const overlay = document.getElementById("overlayConfirm");
+        if (overlay) overlay.style.display = "flex";
+        const h1 = document.createElement('h1')
+        h1.textContent = 'it runed'
+
+        document.querySelector('[data-role="notification"]').appendChild(h1)
+
+        document.getElementById("btnCancelConfirm").onclick = function () {
+            document.getElementById("overlayConfirm").style.display = "none";
+        }
+
+        document.getElementById("btnOkConfirm").onclick = function () {
+            document.getElementById("overlayConfirm").style.display = "none";
+            alert("Đã đồng ý");
+        }
     }
 }
 export default notification
