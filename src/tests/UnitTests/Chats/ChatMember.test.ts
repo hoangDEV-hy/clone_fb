b938a9374581fc009940686ea8f3d5c1b23e38bf
HoangDEV <nhh19072005@gmail.com>
2026-01-30 15:25:47 +0700

    write UnitTest chat_member example

--- src/tests/UnitTests/Chats/ChatMember.test.ts
+++ src/tests/UnitTests/Chats/ChatMember.test.ts
@@ -1,32 +1,32 @@
-jest.mock("../../src/Models/Chats/ChatMember", () => ({
-    selectWhere: jest.fn().mockResolvedValue([{
-        id: "1",
-        name: "John Doe",
-        email: "john.doe@example.com",
-        password: "password",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-    }]) as jest.Mock,
-    add_members: jest.fn().mockResolvedValue([{
-        id: "1",
-        name: "John Doe",
-        email: "john.doe@example.com",
-        password: "password",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-    }]) as jest.Mock,
-}));
+import { methods as selectMemberModel } from "../../../Models/Chats/ChatMember"
+import { select_members, add_members } from "../../../Constrollers/Chats/ChatMembers"
+
+import { Op } from "sequelize";
 
 describe("ChatMemberController", () => {
-    it("should return chat members", async () => {
-        const chatMembers = await chatMemberController.selectWhere({ id: "1" });
-        expect(chatMembers).toEqual([{
-            id: "1",
-            name: "John Doe",
-            email: "john.doe@example.com",
-            password: "password",
-            createdAt: new Date(),
-            updatedAt: new Date(),
-        }]);
+    it("builds correct query from input", async () => {
+        (selectMemberModel.selectWhere as jest.Mock).mockResolvedValue([
+            {
+                id: "1",
+                name: "John Doe",
+                email: "john.doe@example.com",
+                password: "password",
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            },
+        ]);
+
+        await select_members("user 1", "user 2", 1);
+
+        expect(selectMemberModel.selectWhere).toHaveBeenCalledWith(
+            expect.objectContaining({
+                [Op.and]: {
+                    idUser: { [Op.in]: ["user 1", "user 2"] },
+                    chat_id: 1,
+                },
+            })
+        );
     });
 });
+
+
